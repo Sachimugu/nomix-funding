6b6bc82242b5165e3bb992a0e1abb6e97d999593
sachimugu <sachiimugu@gmail.com>
2025-03-16 07:32:23 +0100

    meta

--- client/app/page.js
+++ client/app/page.js
@@ -19,33 +19,7 @@ import HeroSection from "@/components/HeroSection";
 import SponsorsSection from "@/components/Sponsors";
 import { TeamSection } from "@/components/Teams";
 
-export const metadata = {
-  title: "Crowd-Funding",
-  description: "Crowd Funding Platform Built On Web3",
-  openGraph: {
-    type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
-    images: [
-      {
-        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Shadcn - Landing template",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
-    images: [
-      "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-    ],
-  },
-};
+
 
 export default function Home() {
 
